7840bebf16a9f335e3403442cb81651cc4e9c682
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-02-07 00:57:49 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::resource('Produnts',ProductController::class);
+Route::resource('/products', ProductController::class );
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
 
 // Route::get('/products', [ProductController::class, 'index']);
 // Route::post('/products',[ProductController::class, 'store']);
